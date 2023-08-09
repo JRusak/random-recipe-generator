78e34bb6689fb177a0922d33ee38c4f363268905
Jan Rusak <jrusak001@gmail.com>
2023-08-09 14:54:38 +0200

    Updated app

--- random-recipe-fe/src/app/app.module.ts
+++ random-recipe-fe/src/app/app.module.ts
@@ -5,9 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RecipeComponent } from './recipe/recipe.component';
+import { RandomComponent } from './random/random.component';
+import { RandomWithPrefsComponent } from './random-with-prefs/random-with-prefs.component';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
-  declarations: [AppComponent, RecipeComponent],
+  declarations: [AppComponent, RecipeComponent, RandomComponent, RandomWithPrefsComponent, FavouritesComponent, MenuComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
