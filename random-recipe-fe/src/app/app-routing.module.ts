4cbdd4662bfe4bebd411ecbde1e98ba58dd0c23d
Jan Rusak <jrusak001@gmail.com>
2023-08-09 14:52:39 +0200

    Updated rooting

--- random-recipe-fe/src/app/app-routing.module.ts
+++ random-recipe-fe/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RandomComponent } from './random/random.component';
+import { FavouritesComponent } from './favourites/favourites.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'random-recipe',
+    pathMatch: 'full',
+  },
+  {
+    path: 'random-recipe',
+    component: RandomComponent,
+  },
+  {
+    path: 'favourites',
+    component: FavouritesComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
